e658ddf75ee261eb19bb082c2a109fde8f22690c
Wouter Deloof <wouter.deloof@gmail.com>
2020-01-24 09:44:56 +0100

    Initial Commit

---
+++ SwiftAlgemeneHerhaling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAlgemeneHerhaling
+//
+//  Created by mobapp15 on 24/01/2020.
+//  Copyright © 2020 mobapp15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
